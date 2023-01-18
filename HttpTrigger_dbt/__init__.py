5b72d1f1a89e5cdd708f13028fe10064a81f3b28
mark.james.1 <mark.james.1@canopius.com>
2023-01-18 12:42:02 +0000

    typo

--- HttpTrigger_dbt/__init__.py
+++ HttpTrigger_dbt/__init__.py
@@ -8,7 +8,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Start With Popen dbt')
     #with subprocess.Popen(["ls"], stdout=subprocess.PIPE, shell=True) as proc: # this runs fine, although shell cmd shell=True 
-    with subprocess.Popen(["/tmp", "dbt"], stdout=subprocess.PIPE) as proc: # Attempt to output dbt helper as doesn't write to log- When running locally remove subprocess.Popen(["dbt"] remove "/tmp"
+    with subprocess.Popen(["/tmp", "dbt"], stdout=subprocess.PIPE) as proc: # Attempt to output dbt helper as doesn't write to log- When running locally subprocess.Popen(["dbt"] remove "/tmp"
         for line in proc.stdout:
             line = line.decode('utf-8').replace('\n', '').strip()
             logging.info(line)
